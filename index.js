7fa9c442684b78639d4c7cc8eadcebaf05ab12dc
Tyler Watson <tyler@Tylers-MacBook.local>
2019-05-28 12:14:53 -0700

    added passport callbacks, completed auth workflow, encode/serialize/deserialize users, enabled cookie based auth, added auth route

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 const express = require('express');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+require('./models/User');
+require('./services/passport');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+
+mongoose.connect(keys.mongoURI);
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ bye: 'buddy' });
-});
+//enable cookies inside our application
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+
+//tells passport we're making use of cookies
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
